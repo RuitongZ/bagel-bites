73f073bb729faeaa477bb8761ccdaabf83ad04f9
CaesarHp <ryllazhu@gmail.com>
2024-05-31 18:48:28 -0400

    Component changes

--- src/ui/ScrollToTop.js
+++ src/ui/ScrollToTop.js
@@ -1,51 +1,44 @@
 import React from 'react';
 
-import { styled } from '@mui/material/styles';
-import { Box, IconButton } from '@mui/material';
+// import { styled } from '@mui/material/styles';
+// import IconButton from '@mui/material/IconButton';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
-
-const ScrollToTopBtn = styled(IconButton)({
-  backgroundColor: '#000',
-  borderRadius: '50%',
-  right: '30px',
-  bottom: '60px',
-  ':hover': { backgroundColor: '#fff' },
-  width: {
-    xs: '30px',
-    xl: '60px',
-  },
-  height: {
-    xs: '30px',
-    xl: '60px',
-  },
-});
+import { Fab, Zoom, useScrollTrigger } from '@mui/material';
 
 const iconStyles = {
+  boxShadow: 'none',
+  position: 'fixed',
+  bottom: 20,
+  right: 20,
   color: '#fff',
-  backgroundColor: 'transparent',
-  ':hover': { color: '#FF5500', backgroundColor: 'transparent' },
-  borderRadius: '50%',
-  fontSize: {
-    xs: 30,
-    sm: 30,
-    md: 30,
-    lg: 30,
-    xl: 40,
-  },
+  backgroundColor: '#000',
+  '&:hover': { color: '#FF5500', backgroundColor: '#fff' },
+  width: { xs: 40, sm: 45, md: 50, lg: 50, xl: 70 },
+  height: { xs: 40, sm: 45, md: 50, lg: 50, xl: 70 },
 };
+
 export default function ScrollToTop() {
+  const trigger = useScrollTrigger({
+    disableHysteresis: true,
+    threshold: 100,
+  });
+
+  const handleClick = () => {
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  };
+
   return (
-    <Box
-      sx={{
-        position: 'sticky',
-        width: '100%',
-        display: 'flex',
-        justifyContent: 'flex-end',
-      }}
-    >
-      <ScrollToTopBtn>
-        <KeyboardArrowUpIcon sx={iconStyles} />
-      </ScrollToTopBtn>
-    </Box>
+    <Zoom in={trigger}>
+      <Fab
+        onClick={handleClick}
+        aria-label='scroll to top'
+        disableRipple
+        sx={iconStyles}
+      >
+        <KeyboardArrowUpIcon
+          sx={{ fontSize: { xs: 20, sm: 25, md: 30, lg: 35, xl: 40 } }}
+        />
+      </Fab>
+    </Zoom>
   );
 }
