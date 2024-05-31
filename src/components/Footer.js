ca171c630a1da745c4885172cc1f2d13ba166ce9
CaesarHp <ryllazhu@gmail.com>
2024-05-31 18:47:24 -0400

    Style changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,6 @@ import { styled } from '@mui/material/styles';
 
 const OrangeContainer = styled(Box)({
   minWidth: '100%',
-  minHeight: '50vh',
   backgroundColor: '#FF5500',
   display: 'flex',
   flexDirection: 'column',
@@ -13,39 +12,53 @@ const OrangeContainer = styled(Box)({
   alignItems: 'center',
 });
 
-const FooterBox = styled(Box)({
+const footerBoxStyles = {
   display: 'flex',
   justifyContent: 'center',
   minWidth: '100%',
-  marginBottom: '100px',
-});
-
-const footerBoxStyles = {
   flexDirection: { xs: 'column', sm: 'row' },
-  paddingX: { xs: '30px', sm: '50px', md: '80px', lg: '100px' },
+  padding: { xs: '40px', sm: '50px', md: '60px', lg: '80px' },
 };
 
 const menuBoxStyles = {
   display: 'flex',
-  flexDirection: { xs: 'row', sm: 'column', md: 'column' },
+  flexDirection: { xs: 'row', sm: 'column' },
   alignItems: 'flex-start',
-  width: { xs: '100%', sm: '100%', md: '50%' },
+  justifyContent: 'space-between',
+  width: { xs: '100%', sm: '25%' },
 };
 
-const MenuBtn = styled(Button)({
+const menuBtn = {
   color: '#000',
   textTransform: 'none',
   ':hover': {
     backgroundColor: 'transparent',
   },
-});
+  padding: 0,
+  pb: '10px',
+  fontSize: { xs: 12, sm: 12, md: 15, lg: 20, xl: 25 },
+  justifyContent: 'flex-start',
+};
+
+const subscribeBox = {
+  width: { xs: '100%', sm: '60%' },
+  paddingLeft: { xs: 0, sm: '50px', md: '100px', lg: '200px' },
+  mt: { xs: 4, sm: 0 },
+};
+
+const subTypo = {
+  fontSize: { xs: 14, sm: 12, md: 15, lg: 20, xl: 25 },
+  mb: 1,
+  display: 'flex',
+  justifyContent: 'center',
+};
 
 const subscribeBtn = {
+  paddingX: '40px',
   backgroundColor: '#000',
   textTransform: 'none',
   fontSize: { md: 16, lg: 18, xl: 20 },
   height: '50px',
-  width: '150px',
   borderRadius: '0 5px 5px 0',
   ':hover': {
     backgroundColor: '#000',
@@ -69,91 +82,61 @@ export default function Footer() {
   const footerMenu2 = ['Contact', 'About Us', 'Reviews & Press'];
   return (
     <OrangeContainer>
-      <FooterBox sx={footerBoxStyles}>
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: { xs: 'column', sm: 'row' },
-            justifyContent: { xs: 'center', sm: 'center', md: 'flex-start' },
-            width: { xs: '100%', sm: '100%', md: '50%' },
-            gap: { xs: '10px', sm: 0, md: '50px' },
-          }}
-        >
-          <Box sx={menuBoxStyles}>
-            {footerMenu1.map((menu) => (
-              <MenuBtn
-                disableRipple
-                key={menu}
-                sx={{
-                  fontSize: { md: 15, lg: 20, xl: 25 },
-                  paddingRight: { xs: '50px' },
-                }}
-              >
-                {menu}
-              </MenuBtn>
-            ))}
-          </Box>
-
-          <Box sx={menuBoxStyles}>
-            {footerMenu2.map((menu) => (
-              <MenuBtn
-                disableRipple
-                key={menu}
-                sx={{
-                  fontSize: { md: 15, lg: 20, xl: 25 },
-                  paddingRight: { xs: '50px' },
-                }}
-              >
-                {menu}
-              </MenuBtn>
-            ))}
-          </Box>
+      <Box sx={footerBoxStyles}>
+        <Box sx={menuBoxStyles}>
+          {footerMenu1.map((menu) => (
+            <Button disableRipple key={menu} sx={menuBtn}>
+              {menu}
+            </Button>
+          ))}
         </Box>
 
-        <Box
-          sx={{
-            width: { xs: '100%', sm: '100%', md: '50%', lg: '60%' },
-            display: 'flex',
-            justifyContent: { xs: 'center', sm: 'flex-end' },
-          }}
-        >
-          <Box>
-            <Typography variant='h6'>
-              Subscribe to our newsletter today!
-            </Typography>
-            <Box sx={{ display: 'flex' }}>
-              <TextField
-                variant='filled'
-                id='demo-helper-text-misaligned-no-helper'
-                label='*Please enter your E-mail...'
-                type='emial'
-                sx={{
-                  width: '100%',
+        <Box sx={menuBoxStyles}>
+          {footerMenu2.map((menu) => (
+            <Button disableRipple key={menu} sx={menuBtn}>
+              {menu}
+            </Button>
+          ))}
+        </Box>
+
+        <Box sx={subscribeBox}>
+          <Typography variant='h6' sx={subTypo}>
+            Subscribe to our newsletter today!
+          </Typography>
+
+          <Box sx={{ display: 'flex', width: '100%' }}>
+            <TextField
+              variant='filled'
+              id='demo-helper-text-misaligned-no-helper'
+              label='*Please enter your E-mail...'
+              type='emial'
+              sx={{
+                width: '100%',
+                height: '50px',
+                backgroundColor: '#fff',
+                borderRadius: '5px 0 0 5px',
+              }}
+              InputProps={{
+                style: {
                   height: '50px',
-                  backgroundColor: '#fff',
-                  borderRadius: '5px 0 0 5px',
-                }}
-                InputProps={{
-                  style: {
-                    height: '50px',
-                  },
-                }}
-                InputLabelProps={{
-                  style: { color: '#000' },
-                }}
-              />
-              <Button
-                disableRipple
-                variant='contained'
-                disableElevation
-                sx={subscribeBtn}
-              >
-                Subscribe
-              </Button>
-            </Box>
+                },
+              }}
+              InputLabelProps={{
+                style: { color: '#000' },
+              }}
+            />
+            <Button
+              disableRipple
+              variant='contained'
+              disableElevation
+              sx={subscribeBtn}
+            >
+              Subscribe
+            </Button>
           </Box>
         </Box>
-      </FooterBox>
+      </Box>
+
       <Typography variant='h6' sx={copyrightStyles}>
         &#169; Bagel Bites. All rights reserve.
       </Typography>
