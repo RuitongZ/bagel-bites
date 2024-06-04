4b8830287f85d21bb04e24214ae7f041da7c5ee1
CaesarHp <ryllazhu@gmail.com>
2024-06-04 17:54:26 -0400

    responsive setting

--- src/sections/Welcome.js
+++ src/sections/Welcome.js
@@ -27,50 +27,96 @@ const ColorFilter = styled(Box)({
   top: 0,
 });
 
+const TextGridStyles = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'flex-end',
+  minWidth: '100%',
+  minHeight: { xs: '50vh', sm: '100vh' },
+};
+
+const grid1Styles = {
+  display: 'flex',
+  justifyContent: 'center',
+  marginTop: {
+    xs: '250px',
+    sm: '350px',
+    md: '300px',
+    lg: '320px',
+    xl: '500px',
+  },
+};
+
+const grid2Styles = {
+  display: 'flex',
+  justifyContent: 'center',
+};
+
+const grid3Styles = {
+  display: {
+    xs: 'flex',
+    sm: 'none',
+  },
+  justifyContent: 'center',
+};
+
+const grid4Styles = {
+  display: {
+    xs: 'none',
+    sm: 'flex',
+  },
+  justifyContent: 'center',
+};
+
+const welcomeStyles = {
+  height: {
+    xs: '40px',
+    sm: '70px',
+    md: '75px',
+    lg: '70px',
+    xl: '100px',
+  },
+  color: '#fff',
+  fontSize: { xs: 60, sm: 80, md: 100, lg: 110, xl: 150 },
+};
+
+const verticalLineStyles = {
+  height: {
+    xs: '90px',
+    sm: '110px',
+    md: '120px',
+    lg: '100px',
+    xl: '180px',
+  },
+  borderLeft: 1,
+  borderColor: '#fff',
+};
+
+const smLogoStyles = {
+  pl: { xs: '40px' },
+  pr: { xs: '40px' },
+  width: '100%',
+  height: 'auto',
+};
+
+const lgLogoStyles = {
+  pl: { xs: '60px' },
+  pr: { xs: '60px' },
+  width: '100%',
+  height: 'auto',
+};
+
 export default function Welcome() {
   return (
     <BackgroundContainer>
       <ColorFilter>
-        <Grid
-          container
-          spacing={3}
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'flex-end',
-            minWidth: '100%',
-            minHeight: { xs: '50vh', sm: '100vh' },
-          }}
-        >
+        <Grid container spacing={3} sx={TextGridStyles}>
           <Grid //welcome
             item
             xs={12}
-            sx={{
-              display: 'flex',
-              justifyContent: 'center',
-              marginTop: {
-                xs: '250px',
-                sm: '350px',
-                md: '300px',
-                lg: '320px',
-                xl: '500px',
-              },
-            }}
+            sx={grid1Styles}
           >
-            <Typography
-              variant='h3'
-              sx={{
-                height: {
-                  xs: '40px',
-                  sm: '70px',
-                  md: '75px',
-                  lg: '70px',
-                  xl: '100px',
-                },
-                color: '#fff',
-                fontSize: { xs: 60, sm: 80, md: 100, lg: 110, xl: 150 },
-              }}
-            >
+            <Typography variant='h3' sx={welcomeStyles}>
               Welcome
             </Typography>
           </Grid>
@@ -78,69 +124,34 @@ export default function Welcome() {
           <Grid //line
             item
             xs={12}
-            sx={{ display: 'flex', justifyContent: 'center' }}
+            sx={grid2Styles}
           >
-            <Box
-              component='span'
-              sx={{
-                height: {
-                  xs: '90px',
-                  sm: '110px',
-                  md: '120px',
-                  lg: '100px',
-                  xl: '180px',
-                },
-                borderLeft: 1,
-                borderColor: '#fff',
-              }}
-            />
+            <Box component='span' sx={verticalLineStyles} />
           </Grid>
 
           <Grid //Bagel Bites
             item
             xs={12}
-            sx={{
-              display: {
-                xs: 'flex',
-                sm: 'none',
-              },
-              justifyContent: 'center',
-            }}
+            sx={grid3Styles}
           >
             <Box
               component='img'
               src={BrandLogo}
               alt='Brand logo'
-              sx={{
-                pl: { xs: '40px' },
-                pr: { xs: '40px' },
-                width: '100%',
-                height: 'auto',
-              }}
+              sx={smLogoStyles}
             />
           </Grid>
 
           <Grid //Bagel Bites
             item
             xs={12}
-            sx={{
-              display: {
-                xs: 'none',
-                sm: 'flex',
-              },
-              justifyContent: 'center',
-            }}
+            sx={grid4Styles}
           >
             <Box
               component='img'
               src={BrandLogoCut}
               alt='Bagel Bites'
-              sx={{
-                pl: { xs: '60px' },
-                pr: { xs: '60px' },
-                width: '100%',
-                height: 'auto',
-              }}
+              sx={lgLogoStyles}
             />
           </Grid>
         </Grid>
